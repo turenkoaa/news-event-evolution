6590ee0f47991a8ef4470ed88bd81cd583788bf2
turenkoaa <aaturenko@gmail.com>
2019-03-09 17:51:19 +0300

    filtering news done

--- read_news.py
+++ read_news.py
@@ -3,6 +3,7 @@ import json
 import os
 from langdetect import detect_langs
 
+
 def is_lang(text, lang, prob): # print(is_lang("Smth", "ru", 0.75))
     try:
         list_of_languages = detect_langs(text)
@@ -11,20 +12,27 @@ def is_lang(text, lang, prob): # print(is_lang("Smth", "ru", 0.75))
         print("error in detecting language for text: \"" + text + "\"")
         return False
 
+def filter_doc(doc, spam):
+    return doc["clusterId"] not in spam and is_lang(doc["vanilla"], "ru", 0.70)
+    # return doc["officialGroup"] and doc["clusterId"] not in spam and is_lang(doc["vanilla"], "ru", 0.70)
+
 def get_file_for_date(dirname):
     filename = os.listdir(dirname)[0]
     root, ext = os.path.splitext(filename)
     return dirname + filename if ext == '.json' else None
 
-def get_russian_news(date):
-    filepath = get_file_for_date("C:/Users/User/Desktop/diploma/datasets/outerTrendsDataset/texts/date=" + date + "/")
-    print("processing file: " + filepath)
-    with open(filepath, encoding="utf8") as f:
-        lines = f.readlines()
-        news = list(map(lambda line: json.loads(line), lines))
-        russian_news = list(filter(lambda n: is_lang(n['vanilla'], "ru", 0.70), news))
-        print(str(len(russian_news)) + "/" + str(len(lines)) + " russian news were found for date=" + date)
-        return russian_news
+def get_spam_clusters(date, threshold):
+    trends_path = get_file_for_date("C:/Users/User/Desktop/diploma/datasets/outerTrendsDataset/trends/date=" + date + "/")
+    print("processing for spam file: " + trends_path)
+    with open(trends_path, encoding="utf8") as trends:
+        spam = []
+        trends_lines = trends.readlines()
+        for trends_line in trends_lines:
+            trends_json = json.loads(trends_line)
+            if trends_json["percentIsSpam"] > threshold:
+                spam.append(trends_json["clusterId"])
+        print(str(len(spam)) + " spam clusters were found")
+        return spam
 
 def get_dates_between(d1, d2):
     delta = d2 - d1         # timedelta
@@ -33,6 +41,48 @@ def get_dates_between(d1, d2):
         dt = d1 + datetime.timedelta(i)
         dates.append(dt.strftime("%Y-%m-%d"))
     return dates
+
+def get_filtered_news(date):
+    spam = get_spam_clusters(date, 0.5)
+    filepath = get_file_for_date("C:/Users/User/Desktop/diploma/datasets/outerTrendsDataset/texts/date=" + date + "/")
+    print("processing file: " + filepath)
+    with open(filepath, encoding="utf8") as f:
+        lines = f.readlines()
+        news = list(map(lambda line: json.loads(line), lines))
+        filtered_news = list(filter(lambda n: filter_doc(n, spam), news))
+        print(str(len(filtered_news)) + "/" + str(len(lines)) + " filtered news were found for date=" + date)
+        return filtered_news
+
+def read_cluster(news, cluster_id):
+    return list(filter(lambda n: n["clusterId"] == cluster_id, news))
+
+
+def get_filtered_news_file(date):
+    return "C:/Users/User/Desktop/diploma/ner/data/filtered_news_" + date
+
+
+def write_filtered_news_to_file(date):
+    news = get_filtered_news(date)
+    with open(get_filtered_news_file(date), 'w', encoding="utf8") as fp:
+        json.dump(news, fp, ensure_ascii=False)
+
+
+def read_filtered_news_from_file(date):
+    with open(get_filtered_news_file(date), 'r', encoding="utf8") as fp:
+        return json.load(fp)
+
+# d1 = datetime.date(2018, 10, 10)  # start date
+# d2 = datetime.date(2018, 12, 31)  # end date
+# dates = get_dates_between(d1, d2)
+
+
+# news = read_filtered_news_from_file(d1)
+# for i in range(1,5):
+#     cluster = read_cluster(news, i)
+#     for doc in cluster:
+#         print(doc["vanilla"])
+#         print("// cluster_id=" + str(doc["clusterId"]))
+#     print("____________________________________________________________")
 
 
 
